5f93bdc234eaa7a8b14176b66924850efc4daa68
Florian Fontan <dev@florian-fontan.fr>
2026-06-07 00:12:06 +0200

    Improve total tardiness in tree_search_pfss

--- include/shopschedulingsolver/algorithms/tree_search_pfss.hpp
+++ include/shopschedulingsolver/algorithms/tree_search_pfss.hpp
@@ -5,8 +5,36 @@
 namespace shopschedulingsolver
 {
 
+struct TreeSearchPfssParameters: Parameters
+{
+    Counter minimum_size_of_the_queue = 1;
+    Counter maximum_size_of_the_queue = 100000000;
+
+    virtual int format_width() const override { return 37; }
+
+    virtual void format(std::ostream& os) const override
+    {
+        Parameters::format(os);
+        int width = format_width();
+        //os
+        //    << std::setw(width) << std::left << "Max. # of iterations: " << maximum_number_of_iterations << std::endl
+        //    << std::setw(width) << std::left << "Max. # of iterations without impr.:  " << maximum_number_of_iterations_without_improvement << std::endl
+        //    ;
+    }
+
+    virtual nlohmann::json to_json() const override
+    {
+        nlohmann::json json = Parameters::to_json();
+        //json.merge_patch({
+        //        {"MaximumNumberOfIterations", maximum_number_of_iterations},
+        //        {"MaximumNumberOfIterationsWithoutImprovement", maximum_number_of_iterations_without_improvement},
+        //        });
+        return json;
+    }
+};
+
 Output tree_search_pfss(
         const Instance& instance,
-        const Parameters& parameters = {});
+        const TreeSearchPfssParameters& parameters = {});
 
 }
